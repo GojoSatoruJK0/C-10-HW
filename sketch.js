fa7b0379d89e60f8c6265cb2f2e5cb0b15756972
TanjiroRengoku <101446472+TanjiroRengoku@users.noreply.github.com>
2022-05-13 15:06:40 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,11 +2,11 @@ var sea,ship;
 var seaImg,shipImg;
 function preload(){
 //uncomment the code to add animation to ship 
-shipImg1 = loadAnimation("ship-1.png");
-shipImg1 = loadAnimation("ship-1.png");
-shipImg1 = loadAnimation("ship-1");
+//shipImg1 = loadAnimation("ship-1.png");
+//shipImg1 = loadAnimation("ship-1.png");
+//shipImg1 = loadAnimation("ship-1");
 shipImg1 = loadAnimation("ship-1.png","ship-2.png","ship-1.png","ship-2.png");
-shipImg1 = loadAnimation("ship-1","ship-2","ship-1","ship-2");
+//shipImg1 = loadAnimation("ship-1","ship-2","ship-1","ship-2");
 seaImg = loadImage("sea.png");
 }
 function setup(){
@@ -26,10 +26,10 @@ background(0);
 sea.velocityX = -3;
 //uncomment code to reset the background
 if(sea.x < 0){
-sea.x = 0;
-sea.x = sea.width;
+//sea.x = 0;
+//sea.x = sea.width;
 sea.x = sea.width/8;
-sea.y = height;
+//sea.y = height;
 }
 drawSprites();
 }
